861ab14dc5521b1961c6dd963eb879673f441df4
Nikita Kiselev <kiselev.nikita.e@gmail.com>
2023-03-22 11:41:51 +0700

    Deleted unnecessary Debug.Logs

--- Assets/Scripts/HeartLivesManager.cs
+++ Assets/Scripts/HeartLivesManager.cs
@@ -12,23 +12,12 @@ public class HeartLivesManager : MonoBehaviour
     void Start()
     {
         currentLives = gameManager.currentLives;
-        //FindHeartIcons();
-    }
 
-    private void FindHeartIcons()
-    {
-        for (int i = 0; i < gameObject.transform.childCount; i++)
-        {
-            gameObject.transform.GetChild(i).gameObject.SetActive(true);
-        }
     }
 
     public void DestroyHeartIcons()
     {
         currentLives = gameManager.currentLives;
-        Debug.Log("destroy heart");
-        //gameObject.transform.GetChild(gameObject.transform.childCount - 1).gameObject.SetActive(false);
         gameObject.transform.GetChild(currentLives).gameObject.SetActive(false);
-        Debug.Log(gameObject.transform.GetChild(currentLives));
     }
 }
